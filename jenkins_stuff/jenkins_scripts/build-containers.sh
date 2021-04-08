52b07ce167e990d8a559ea77ee876ce77d94d012
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 19:06:58 +0000

    test

--- jenkins_stuff/jenkins_scripts/build-containers.sh
+++ jenkins_stuff/jenkins_scripts/build-containers.sh
@@ -3,6 +3,7 @@
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF
 sh './logindocker.sh'
 sudo docker login -u lukef2021 -p $PASSWORD
-sudo docker run -d -p 5000:5000 --name frontend lukef2021/frontend:latest 
-sudo docker run -d -p 5001:5001 --name backend lukef2021/backend:latest
+sudo docker network create project-network
+sudo docker run -d --network project-network -p 5000:5000 --name server lukef2021/frontend:latest 
+sudo docker run -d --network project-network -p 5001:5001 --name backend lukef2021/backend:latest
 EOF
