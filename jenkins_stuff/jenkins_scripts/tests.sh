d933f152707e6540f7e172e428cc87a5fe819ea1
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 19:43:04 +0000

    added tests step

--- jenkins_stuff/jenkins_scripts/tests.sh
+++ jenkins_stuff/jenkins_scripts/tests.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
 
+ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF
+
+sudo docker exec frontend pytest tests/test_frontend.py --cov application
+sudo docker exec backend pytest tests/test_backend.py --cov application
+
+EOF
