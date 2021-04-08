bd8f6665ea5f0089343b26aba3d8a5c3f6b9fa46
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 14:37:39 +0000

    test fix

--- jenkins_stuff/jenkins_scripts/put-images-on-testvm.sh
+++ jenkins_stuff/jenkins_scripts/put-images-on-testvm.sh
@@ -1,4 +1,4 @@
-ssh 18.133.247.125 << EOF 
+ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF 
 sudo docker pull frontend:latest
 sudo docker pull backend:latest
 EOF 
