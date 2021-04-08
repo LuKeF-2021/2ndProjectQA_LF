fcfd9eb9d19458525a9dda9d34986ac4b1993a4b
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 20:04:17 +0000

    fixed jenkinfiles

--- jenkins_stuff/jenkins_scripts/tests.sh
+++ jenkins_stuff/jenkins_scripts/tests.sh
@@ -2,7 +2,10 @@
 
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF
 
-sudo docker exec frontend pytest tests/test_frontend.py --cov application
-sudo docker exec backend pytest tests/test_backend.py --cov application
+sudo docker exec frontend pytest tests/test_frontend.py --cov application >> frontend_results.txt
+sudo docker exec backend pytest tests/test_backend.py --cov application >> backend_results.txt
+
+cat ~/frontend_results.txt
+cat ~/backend_results.txt
 
 EOF
