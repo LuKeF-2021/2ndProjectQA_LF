71a992899815a13263f5ead43d3f52c7ec106ca7
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 17:25:42 +0000

    fixed

---
+++ jenkins_stuff/jenkins_scripts/remove-previous-containers.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF 
+sudo docker stop frontend
+sudo docker stop backend
+
+sudo docker rm frontend
+sudo docker rm backend
+EOF
