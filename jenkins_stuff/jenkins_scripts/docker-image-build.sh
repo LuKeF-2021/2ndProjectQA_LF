163b4c2568720c17de66ffda84d59e790f46fb1d
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 10:28:21 +0000

    added docker stuff

---
+++ jenkins_stuff/jenkins_scripts/docker-image-build.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo "Docker image building in process"
+
+# finding frontend/Dockerfile
+echo "build frontend image started"
+cd ..
+cd frontend
+sudo docker build -t frontend:latest
+cd ..
+
+# finding backend/Dockerfile
+echo "build backend image started"
+cd backend
+sudo docker build -t backend:latest
+
+# builds complete by this stage, time to push images to DockerHub
+
+echo "Pushing images to DockerHub"
+
+sudo docker tag frontend:latest lukef2021/frontend:latest
+sudo docker tag backend:latest lukef2021/backend:latest
+sudo docker push lukef2021/frontend:latest
+sudo docker push lukef2021/backend:latest
+
+echo "images pushed, deleting local images"
+
+sudo docker rmi frontend:latest
+sudo docker rmi backend:latest
+sudo docker rmi lukef2021/frontend:latest
+sudo docker rmi lukef2021/backend:latest
