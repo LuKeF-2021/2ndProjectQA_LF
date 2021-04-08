812132800b860f74d558a66201696db80f43c6c5
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 16:20:53 +0000

    test fix

--- jenkins_stuff/jenkins_scripts/build-containers.sh
+++ jenkins_stuff/jenkins_scripts/build-containers.sh
@@ -3,10 +3,10 @@
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF
 sh './logindocker.sh'
 sudo docker login -u lukef2021 -p $PASSWORD
-cd ~/jenkins_stuff/frontend
+cd ./jenkins_stuff/frontend
 sudo docker run -d -p 5000:5000 --name frontend:latest frontend:latest
 cd ..
 
-cd ~/jenkins_stuff/backend
+cd ./jenkins_stuff/backend
 sudo docker run -d -p 5001:5001 --name backend:latest backend:latest
 EOF
