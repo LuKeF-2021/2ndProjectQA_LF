24fd87082530298c723c799eba93d1c4e56a0f23
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 22:01:45 +0000

    updated

--- jenkins_stuff/jenkins_scripts/put-images-on-testvm.sh
+++ jenkins_stuff/jenkins_scripts/put-images-on-testvm.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@35.176.207.137 << EOF
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@35.176.207.137 << EOF
 sh './logindocker.sh'
 sudo docker login -u lukef2021 -p $PASSWORD
 sudo docker pull lukef2021/frontend:latest
--- jenkins_stuff/jenkins_scripts/tests.sh
+++ jenkins_stuff/jenkins_scripts/tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@35.176.207.137 << EOF
+ssh -o StrictHostKeyChecking=no -i /home/ubuntu/.ssh/id_rsa ubuntu@35.176.207.137 << EOF
 
 sudo docker exec frontend pytest tests/test_frontend.py --cov application >> frontend_results.txt
 sudo docker exec backend pytest tests/test_backend.py --cov application >> backend_results.txt
