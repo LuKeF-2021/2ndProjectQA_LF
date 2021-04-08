1b0ba3db3b49470d87cecb58faadbc20a5c413b8
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 14:53:37 +0000

    test changes

--- jenkins_stuff/jenkins_scripts/put-images-on-testvm.sh
+++ jenkins_stuff/jenkins_scripts/put-images-on-testvm.sh
@@ -1,4 +1,6 @@
 ssh -o StrictHostKeyChecking=no -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.247.125 << EOF 
+sh './logindocker.sh'
+sudo docker login -u lukef2021 -p $PASSWORD
 sudo docker pull frontend:latest
 sudo docker pull backend:latest
 EOF 
