14febeb261c7040702c1b70274bd1447a9091ab1
Ubuntu <ubuntu@ip-172-31-22-155.eu-west-2.compute.internal>
2021-04-08 13:32:56 +0000

    added login

--- jenkins_stuff/jenkins_scripts/docker-image-build.sh
+++ jenkins_stuff/jenkins_scripts/docker-image-build.sh
@@ -20,6 +20,7 @@ echo "Pushing images to DockerHub"
 
 sudo docker tag frontend:latest lukef2021/frontend:latest
 sudo docker tag backend:latest lukef2021/backend:latest
+sudo docker login -u lukef2021 -p R4c3c4r1!
 sudo docker push lukef2021/frontend:latest
 sudo docker push lukef2021/backend:latest
 
